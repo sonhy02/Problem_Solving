5ee88b9fb8de867dbedf70fdce9d4bf28def9e97
sonhy02 <tls1234568@naver.com>
2023-05-09 17:50:44 +0900

    results

--- anything.cpp
+++ anything.cpp
@@ -162,22 +162,17 @@
 //        }
 //    }
 //}
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+#include <stdio.h>
 
-int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n);
-    for (int i = 0; i < n; i++) {
-        int k;
-        cin >> k;
-        v[i] = k;
-    }
-    sort(v.begin(), v.end());
-    for (int i = 0; i < n; i++) {
-        cout << v[i] << endl;
-    }
+
+int main()
+{
+    double celsius; // 섭씨 온도
+
+    printf("섭씨 온도를 화씨 온도로 바꿔주는 프로그램. \n");
+    printf("섭씨 온도를 입력해 주세요 : ");
+    scanf("%lf", &celsius); // 섭씨온도를 입력 받는다
+
+    printf("섭씨 %f 도는 화씨로 %f 도 입니다. \n", celsius, 9 * celsius / 5 + 32);
+    return 0;
 }
