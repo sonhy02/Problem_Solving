d86e6dd8ad457d2d701bcc02af364a2fbe5f830a
sonhy02 <tls1234568@naver.com>
2023-05-09 22:38:55 +0900

    results

--- codeforce_live.cpp
+++ codeforce_live.cpp
@@ -11,19 +11,17 @@ int main() {
     int t;
 
     cin >> t;
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vi p(n * m);
-        for (int i = 0; i < n * m; ++i) {
-            cin >> p[i];
-        }
-        sort(p.begin(), p.end());
-        if (n < m) swap(n, m);
-        int result = max((p[p.size() - 1] - p[0]) * (n - 1) * m + (p[p.size() - 1] - p[1]) * (m - 1),
-                         (p[p.size() - 1] - p[0]) * (n - 1) * m + (p[p.size() - 2] - p[0]) * (m - 1));
 
-        cout << result << "\n";
+
+    while (t--) {
+
+
+
+
+
+
+
+
     }
     return 0;
 }
