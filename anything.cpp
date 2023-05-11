42acea6f95cef1e4dfe5be9aac88ea4557916dda
sonhy02 <tls1234568@naver.com>
2023-05-12 01:28:40 +0900

    results

--- anything.cpp
+++ anything.cpp
@@ -162,5 +162,33 @@
 //        }
 //    }
 //}
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+int N;
+int arr[1001];
+int DP[1001];
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N;
+    for (int i = 1; i <= N; i++) cin >> arr[i];
+
+
+    int Answer = 0;
+    for (int i = 1; i <= N; i++) {
+        DP[i] = 1;
+        for (int j = i - 1; j >= 1; j--) {
+            if (arr[i] > arr[j]) {
+                DP[i] = max(DP[i], DP[j] + 1);
+            }
+        }
+        Answer = max(DP[i], Answer);
+    }
+
+    cout << Answer << endl;
+
+}
