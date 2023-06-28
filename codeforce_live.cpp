27df3ce6117b24faf80c49b03fb442c99be70e25
sonhy02 <tls1234568@naver.com>
2023-06-28 14:40:47 +0900

    results

--- codeforce_live.cpp
+++ codeforce_live.cpp
@@ -10,13 +10,28 @@ using vi = vector<int>;
 using vll = vector<ll>;
 using vpii = vector<pii>;
 
+vector<pair<int, string>> block;
+vi chain;
+bool check[100001];
+
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < m; ++i) {
+        string s1, s2;
+        int t;
+        cin >> s1 >> s2 >> t;
+        block.push_back({t, s1});
+        check[t] = true;
+    }
+    for (int i = 1; i < 2 * n; ++i) {
+        if (!check[i])
+            chain.push_back(i);
+    }
 
     return 0;
 }
-
