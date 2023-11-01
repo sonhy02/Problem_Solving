37421f47840834b605db4cea26655752f06b5d9f
sonhy02 <tls1234568@naver.com>
2023-11-01 11:41:46 +0900

    results

--- baekjoon.cpp
+++ baekjoon.cpp
@@ -11,7 +11,6 @@ using vll = vector<ll>;
 using vpii = vector<pii>;
 
 
-int dp[101][10][1024];
 
 int main() {
     ios::sync_with_stdio(false);
@@ -21,31 +20,9 @@ int main() {
     freopen("../input.txt", "r", stdin);
     freopen("../output.txt", "w", stdout);
 #endif
-    int n;
-    cin >> n;
-    for (int i = 1; i < 10; ++i) {
-        dp[1][i][(1 << i)] = 1;
-    }
-    for (int i = 2; i <= n; ++i) { // n열
-        for (int j = 0; j < 10; ++j) {  // 마지막 번호
-            for (int bit = 0; bit < (1 << 10); ++bit) {
-                if (j == 0) {
-                    dp[i][j][bit | 1 << j] += dp[i - 1][j + 1][bit] % (int)10e8;
-                } else if (j == 9) {
-                    dp[i][j][bit | 1 << j] += dp[i - 1][j - 1][bit] % (int)10e8;
-                } else {
-                    dp[i][j][bit | 1 << j] += dp[i - 1][j - 1][bit]  % (int)10e8;
-                    dp[i][j][bit | 1 << j] +=  dp[i - 1][j + 1][bit] % (int)10e8;
-                }
-            }
-        }
-    }
 
-    ll sum = 0;
-    for (int i = 0; i < 10; ++i) {
-        sum += dp[n][i][1023] % (int)10e8;
-    }
-    cout << sum % (int)10e8;
+
+
 
     return 0;
 
