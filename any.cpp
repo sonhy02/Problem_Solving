4054cf6e176316e0957149431079501c3fd7e3f5
son hoyoung <60101129+sonhy02@users.noreply.github.com>
2024-05-24 13:24:47 +0900

    any.cpp 업데이트

--- any.cpp
+++ any.cpp
@@ -1,76 +1,186 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll = long long;
-using ull = unsigned long long;
-using vll = vector<ll>;
-using vvll = vector<vll>;
-
-int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
-
-    ll n; cin >> n;
-    string s, t; cin >> s >> t;
-
-    set<char> st;
-    for (char c : s) st.insert(c);
-    for (char c : t) {
-        if (!st.count(c)) {
-            cout << 0;
-            return 0;
-        }
-    }
-
-    vvll psum(26, vll(s.size()));
-    for (int c = 0; c < 26; c++) {
-        psum[c][0] = (s[0]-'a' == c);
-        for (int i = 1; i < s.size(); i++) psum[c][i] = psum[c][i-1] + (s[i]-'a' == c);
-    }
-
-    auto func = [&] (ull cycle, ll begin, char target, ull count) -> pair<ull, ll> {
-        auto &p = psum[target-'a'];
-
-        ull what = count + (begin-1 >= 0 ? p[begin-1] : 0);
-        auto it = lower_bound(p.begin()+begin, p.end(), what);
-        if (it != p.end()) return pair<ull, ll>{cycle, it-p.begin()};
-
-        cycle++;
-        count -= p[s.size()-1] - (begin-1 >= 0 ? p[begin-1] : 0);
-
-        ull whole = p[s.size()-1];
-        cycle += count / whole;
-        count %= whole;
-        if (count == 0) {
-            cycle--;
-            it = lower_bound(p.begin(), p.end(), whole);
-            return pair<ull, ll>{cycle, it-p.begin()};
-        }
-
-        it = lower_bound(p.begin(), p.end(), count);
-        return pair<ull, ll>{cycle, it-p.begin()};
-    };
-
-    ull lo = 0, hi = 2e18;
-    while (lo+1 < hi) {
-        ull mid = lo+hi>>1;
-
-        ull cycle = 1;
-        ll begin = 0;
-        for (char c : t) {
-            auto [ncycle, nend] = func(cycle, begin, c, mid);
-            cycle = ncycle;
-            begin = nend+1;
-            if (begin == n) {
-                cycle++;
-                begin = 0;
-            }
-        }
-
-        begin--;
-        if (begin < 0) cycle--;
-
-        if (cycle > n) hi = mid;
-        else lo = mid;
-    }
-    cout << lo;
-}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//#define NOITEM 0
+//#define ISITEM 1
+//#define AVAILABLE 2
+//
+//struct entry {
+//	int key;
+//	string value;
+//	int valid;
+//
+//	entry() {
+//		key = 0;
+//		value = "";
+//		valid = NOITEM;
+//	}
+//	entry(int key, string value) {
+//		this->key = key;
+//		this->value = value;
+//		valid = ISITEM;
+//	}
+//	void erase() {
+//		valid = AVAILABLE;
+//	}
+//};
+//
+//class hashTable {
+//private:
+//	entry* table;
+//	int capacity;
+//	int hashFnc(int key) {
+//		return key % capacity;
+//	}
+//public:
+//	hashTable(int N) {
+//		capacity = N;
+//		table = new entry[capacity];
+//		return;
+//	}
+//	void put(int key, string value) {
+//		int index = hashFnc(key);
+//		int probe = 1;
+//
+//		while (table[index].valid == ISITEM && probe <= capacity) {
+//			index = hashFnc(index + 1);
+//			probe++;
+//		}
+//		if (probe > capacity) {
+//			return;
+//		}
+//		table[index] = entry(key, value);
+//		return;
+//	}
+//	void erase(int key) {
+//		int index = hashFnc(key);
+//		int probe = 1;
+//
+//		while (table[index].valid != NOITEM && probe <= capacity) {
+//			if (table[index].valid == ISITEM && table[index].key == key) {
+//				table[index].erase();
+//				return;
+//			}
+//			index = hashFnc(index + 1);
+//			probe++;
+//		}
+//		return;
+//	}
+//	string find(int key) {
+//		int index = hashFnc(key);
+//		int probe = 1;
+//
+//		while (table[index].valid != NOITEM && probe <= capacity) {
+//			if (table[index].valid == ISITEM && table[index].key == key) {
+//				return table[index].value;
+//			}
+//			index = hashFnc(index + 1);
+//			probe++;
+//		}
+//		return "";
+//	}
+//};
+//
+//class doubleHashTable {
+//private:
+//	entry* table;
+//	int capacity;
+//	int divisor;
+//	int hashFnc(int key) {
+//		return key % capacity;
+//	}
+//	int hashFnc2(int key) {
+//		return divisor - (key % divisor);
+//	}
+//public:
+//	doubleHashTable(int N, int M) {
+//		capacity = N;
+//		divisor = M;
+//		table = new entry[capacity];
+//		return;
+//	}
+//	void put(int key, string value) {
+//		int index = hashFnc(key);
+//		int probe = 1;
+//
+//		while (table[index].valid == ISITEM && probe <= capacity) {
+//			index = hashFnc(index + hashFnc2(key));
+//			probe++;
+//		}
+//		if (probe > capacity) {
+//			return;
+//		}
+//		table[index] = entry(key, value);
+//		cout << probe << endl;
+//		return;
+//	}
+//	void erase(int key) {
+//		int index = hashFnc(key);
+//		int probe = 1;
+//
+//		while (table[index].valid != NOITEM && probe <= capacity) {
+//			if (table[index].valid == ISITEM && table[index].key == key) {
+//				cout << table[index].value << endl;
+//				table[index].erase();
+//				return;
+//			}
+//			index = hashFnc(index + hashFnc2(key));
+//			probe++;
+//		}
+//		cout << "None" << endl;
+//		return;
+//	}
+//	string find(int key) {
+//		int index = hashFnc(key);
+//		int probe = 1;
+//
+//		while (table[index].valid != NOITEM && probe <= capacity) {
+//			if (table[index].valid == ISITEM && table[index].key == key) {
+//				return table[index].value;
+//			}
+//			index = hashFnc(index + hashFnc2(key));
+//			probe++;
+//		}
+//		return "None";
+//	}
+//	void vacant() {
+//		int count = 0;
+//		for (int i = 0; i < capacity; i++) {
+//			if (table[i].valid != ISITEM) {
+//				count++;
+//			}
+//		}
+//		cout << count << endl;
+//	}
+//};
+//
+//int main() {
+//	int t, N, M;
+//	cin >> t >> N >> M;
+//	doubleHashTable ht(N, M);
+//	for (int i = 0; i < t; i++) {
+//		string s;
+//		cin >> s;
+//		if (s == "put") {
+//			int key;
+//			string value;
+//			cin >> key >> value;
+//			ht.put(key, value);
+//		}
+//		else if (s == "erase") {
+//			int key;
+//			cin >> key;
+//			ht.erase(key);
+//		}
+//		else if (s == "find") {
+//			int key;
+//			cin >> key;
+//			cout << ht.find(key) << endl;
+//		}
+//		else if (s == "vacant") {
+//			ht.vacant();
+//		}
+//	}
+//	return 0;
+//}
